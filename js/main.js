d2b8f4f51f1946105671127a8f7a41d06747638f
Kimberly Preston <kimberlypreston@Kimberlys-MacBook-Pro-2.local>
2017-05-03 13:40:37 -0600

    fixing shit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+
+
+
+
+$(window).scroll(function(){
+	// alert("scrolled");
+	parallax();
+
+});
+	
+
+
+
+});
+
+function parallax(){
+        // var scrolled = $(window).scrollTop();
+        var scrolled = $(window).scrollTop();
+        // $(".main").css("top", 1+(scrolled * 0.4));
+        // $(".main").css("top", (scrolled*1.2));
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
